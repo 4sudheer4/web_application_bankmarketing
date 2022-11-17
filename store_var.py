9d506090fc6208aed1b0ec89d26c208ee4237de5
4sudheer4 <sudheeranugoju4@gmail.com>
2022-11-17 01:46:19 -0500

    added html files and stored, loaded robust_scalar

---
+++ store_var.py
@@ -0,0 +1,11 @@
+import pickle
+
+# Create a variable
+myvar = [{'This': 'is', 'Example': 2}, 'of',
+		'serialisation', ['using', 'pickle']]
+
+# Open a file and use dump()
+with open('file.pkl', 'wb') as file:
+	
+	# A new file will be created
+	pickle.dump(myvar, file)
